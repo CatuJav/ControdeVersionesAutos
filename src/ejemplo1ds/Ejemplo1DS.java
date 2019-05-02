db00cc16364a943785999a1ac9f5ff74941f1854
sebas <sebas@Lenovo>
2019-05-02 08:21:55 -0500

    Se ha elaborado una impresion en consola

--- src/ejemplo1ds/Ejemplo1DS.java
+++ src/ejemplo1ds/Ejemplo1DS.java
@@ -15,7 +15,9 @@ public class Ejemplo1DS {
      * @param args the command line arguments
      */
     public static void main(String[] args) {
-        // TODO code application logic here
+   
+        //hecho por Sebastian
+        System.out.println("Practica001");
     }
     
 }
