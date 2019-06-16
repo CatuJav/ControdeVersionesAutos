0f4ea82787e467d2a7dec0697c63fbebe4e26ebd
scatucuago8955 <scatucuago8955@uta.edu.ec>
2019-06-15 21:32:59 -0500

    fin

--- src/ejemplo1ds/interfaces/modelo.java
+++ src/ejemplo1ds/interfaces/modelo.java
@@ -10,7 +10,11 @@ import java.sql.PreparedStatement;
 import java.sql.ResultSet;
 import java.sql.SQLException;
 import java.sql.Statement;
+import javax.swing.JInternalFrame;
 import javax.swing.JOptionPane;
+import javax.swing.event.ListSelectionEvent;
+import javax.swing.event.ListSelectionListener;
+import javax.swing.table.DefaultTableModel;
 
 /**
  *
@@ -24,28 +28,44 @@ public class modelo extends javax.swing.JInternalFrame {
     public modelo() {
         initComponents();
         cargarCodigo();
-        cbxMarcas.cargarDatos("viaje", "marca", "MAR_CODIGO","MAR_NOM");
+        cbxMarcas.cargarDatos("viaje", "marca", "MAR_CODIGO", "MAR_NOM");
+        cargarTabla();
+        tblModelos.getTableHeader().setReorderingAllowed(false) ;
+           tblModelos.getSelectionModel().addListSelectionListener(new ListSelectionListener(){
+            @Override
+            public void valueChanged(ListSelectionEvent e) {
+                if (tblModelos.getSelectedRow() != -1) {
+                    int fila = tblModelos.getSelectedRow();
+                    txtCodigoMo.setText(tblModelos.getValueAt(fila, 0).toString());
+                    cbxMarcas.setSelectedItem(tblModelos.getValueAt(fila, 1).toString());
+                    txtModelos.setText(tblModelos.getValueAt(fila, 2).toString());
+                    
+                }
+            }
+    
+    });
     }
 
-   public void cargarCodigo(){
+    public void cargarCodigo() {
         try {
             conexion cc = new conexion();
-            Connection cn= cc.conectar();
-            String sql="";
-            sql="SELECT * FROM MODELO ORDER BY MOD_CODIGO ASC";
+            Connection cn = cc.conectar();
+            String sql = "";
+            sql = "SELECT * FROM MODELO ORDER BY MOD_CODIGO ASC";
             Statement psd = cn.createStatement();
             ResultSet rs = psd.executeQuery(sql);
-            String dato="0";
-            while(rs.next()){
-            dato = rs.getString("MOD_CODIGO");
+            String dato = "0";
+            while (rs.next()) {
+                dato = rs.getString("MOD_CODIGO");
                 System.out.println(dato);
             }
-            txtCodigoMo.setText(String.valueOf(Integer.valueOf(dato)+1));
-            
+            txtCodigoMo.setText(String.valueOf(Integer.valueOf(dato) + 1));
+
         } catch (SQLException ex) {
             JOptionPane.showMessageDialog(this, ex.getMessage());
         }
-   }
+    }
+
     @SuppressWarnings("unchecked")
     // <editor-fold defaultstate="collapsed" desc="Generated Code">//GEN-BEGIN:initComponents
     private void initComponents() {
@@ -60,6 +80,11 @@ public class modelo extends javax.swing.JInternalFrame {
         jPanel1 = new javax.swing.JPanel();
         jButton1 = new javax.swing.JButton();
         jButton2 = new javax.swing.JButton();
+        jButton3 = new javax.swing.JButton();
+        jButton4 = new javax.swing.JButton();
+        jButton5 = new javax.swing.JButton();
+        jScrollPane1 = new javax.swing.JScrollPane();
+        tblModelos = new javax.swing.JTable();
 
         setDefaultCloseOperation(javax.swing.WindowConstants.DISPOSE_ON_CLOSE);
 
@@ -87,15 +112,39 @@ public class modelo extends javax.swing.JInternalFrame {
             }
         });
 
+        jButton3.setText("Modificar");
+        jButton3.addActionListener(new java.awt.event.ActionListener() {
+            public void actionPerformed(java.awt.event.ActionEvent evt) {
+                jButton3ActionPerformed(evt);
+            }
+        });
+
+        jButton4.setText("Eliminar");
+        jButton4.addActionListener(new java.awt.event.ActionListener() {
+            public void actionPerformed(java.awt.event.ActionEvent evt) {
+                jButton4ActionPerformed(evt);
+            }
+        });
+
+        jButton5.setText("Salir");
+        jButton5.addActionListener(new java.awt.event.ActionListener() {
+            public void actionPerformed(java.awt.event.ActionEvent evt) {
+                jButton5ActionPerformed(evt);
+            }
+        });
+
         javax.swing.GroupLayout jPanel1Layout = new javax.swing.GroupLayout(jPanel1);
         jPanel1.setLayout(jPanel1Layout);
         jPanel1Layout.setHorizontalGroup(
             jPanel1Layout.createParallelGroup(javax.swing.GroupLayout.Alignment.LEADING)
             .addGroup(jPanel1Layout.createSequentialGroup()
                 .addContainerGap()
-                .addGroup(jPanel1Layout.createParallelGroup(javax.swing.GroupLayout.Alignment.LEADING)
-                    .addComponent(jButton1)
-                    .addComponent(jButton2))
+                .addGroup(jPanel1Layout.createParallelGroup(javax.swing.GroupLayout.Alignment.LEADING, false)
+                    .addComponent(jButton3, javax.swing.GroupLayout.DEFAULT_SIZE, javax.swing.GroupLayout.DEFAULT_SIZE, Short.MAX_VALUE)
+                    .addComponent(jButton5, javax.swing.GroupLayout.DEFAULT_SIZE, javax.swing.GroupLayout.DEFAULT_SIZE, Short.MAX_VALUE)
+                    .addComponent(jButton4, javax.swing.GroupLayout.DEFAULT_SIZE, javax.swing.GroupLayout.DEFAULT_SIZE, Short.MAX_VALUE)
+                    .addComponent(jButton2, javax.swing.GroupLayout.DEFAULT_SIZE, javax.swing.GroupLayout.DEFAULT_SIZE, Short.MAX_VALUE)
+                    .addComponent(jButton1, javax.swing.GroupLayout.DEFAULT_SIZE, javax.swing.GroupLayout.DEFAULT_SIZE, Short.MAX_VALUE))
                 .addContainerGap(17, Short.MAX_VALUE))
         );
         jPanel1Layout.setVerticalGroup(
@@ -105,8 +154,27 @@ public class modelo extends javax.swing.JInternalFrame {
                 .addComponent(jButton1)
                 .addPreferredGap(javax.swing.LayoutStyle.ComponentPlacement.RELATED)
                 .addComponent(jButton2)
-                .addContainerGap(24, Short.MAX_VALUE))
+                .addPreferredGap(javax.swing.LayoutStyle.ComponentPlacement.RELATED)
+                .addComponent(jButton3)
+                .addPreferredGap(javax.swing.LayoutStyle.ComponentPlacement.RELATED)
+                .addComponent(jButton4)
+                .addPreferredGap(javax.swing.LayoutStyle.ComponentPlacement.RELATED)
+                .addComponent(jButton5)
+                .addContainerGap(8, Short.MAX_VALUE))
         );
+
+        tblModelos.setModel(new javax.swing.table.DefaultTableModel(
+            new Object [][] {
+                {null, null, null, null},
+                {null, null, null, null},
+                {null, null, null, null},
+                {null, null, null, null}
+            },
+            new String [] {
+                "Title 1", "Title 2", "Title 3", "Title 4"
+            }
+        ));
+        jScrollPane1.setViewportView(tblModelos);
 
         javax.swing.GroupLayout layout = new javax.swing.GroupLayout(getContentPane());
         getContentPane().setLayout(layout);
@@ -128,9 +196,12 @@ public class modelo extends javax.swing.JInternalFrame {
                             .addComponent(cbxMarcas, javax.swing.GroupLayout.DEFAULT_SIZE, 98, Short.MAX_VALUE)
                             .addComponent(txtCodigoMo, javax.swing.GroupLayout.PREFERRED_SIZE, 46, javax.swing.GroupLayout.PREFERRED_SIZE)
                             .addComponent(txtModelos, javax.swing.GroupLayout.DEFAULT_SIZE, javax.swing.GroupLayout.DEFAULT_SIZE, Short.MAX_VALUE))
-                        .addPreferredGap(javax.swing.LayoutStyle.ComponentPlacement.RELATED, 52, Short.MAX_VALUE)
+                        .addPreferredGap(javax.swing.LayoutStyle.ComponentPlacement.RELATED, 36, Short.MAX_VALUE)
                         .addComponent(jPanel1, javax.swing.GroupLayout.PREFERRED_SIZE, javax.swing.GroupLayout.DEFAULT_SIZE, javax.swing.GroupLayout.PREFERRED_SIZE)))
                 .addGap(53, 53, 53))
+            .addGroup(layout.createSequentialGroup()
+                .addComponent(jScrollPane1, javax.swing.GroupLayout.PREFERRED_SIZE, 0, Short.MAX_VALUE)
+                .addContainerGap())
         );
         layout.setVerticalGroup(
             layout.createParallelGroup(javax.swing.GroupLayout.Alignment.LEADING)
@@ -152,7 +223,9 @@ public class modelo extends javax.swing.JInternalFrame {
                             .addComponent(jLabel4)
                             .addComponent(txtModelos, javax.swing.GroupLayout.PREFERRED_SIZE, javax.swing.GroupLayout.DEFAULT_SIZE, javax.swing.GroupLayout.PREFERRED_SIZE)))
                     .addComponent(jPanel1, javax.swing.GroupLayout.PREFERRED_SIZE, javax.swing.GroupLayout.DEFAULT_SIZE, javax.swing.GroupLayout.PREFERRED_SIZE))
-                .addContainerGap(132, Short.MAX_VALUE))
+                .addPreferredGap(javax.swing.LayoutStyle.ComponentPlacement.RELATED)
+                .addComponent(jScrollPane1, javax.swing.GroupLayout.PREFERRED_SIZE, 167, javax.swing.GroupLayout.PREFERRED_SIZE)
+                .addContainerGap(javax.swing.GroupLayout.DEFAULT_SIZE, Short.MAX_VALUE))
         );
 
         pack();
@@ -166,8 +239,26 @@ public class modelo extends javax.swing.JInternalFrame {
 
     private void jButton2ActionPerformed(java.awt.event.ActionEvent evt) {//GEN-FIRST:event_jButton2ActionPerformed
         // TODO add your handling code here:
-        this.dispose();
+        limpiar();
+        cargarCodigo();
+        
     }//GEN-LAST:event_jButton2ActionPerformed
+
+    private void jButton5ActionPerformed(java.awt.event.ActionEvent evt) {//GEN-FIRST:event_jButton5ActionPerformed
+        // TODO add your handling code here:
+        this.dispose();
+    }//GEN-LAST:event_jButton5ActionPerformed
+
+    private void jButton4ActionPerformed(java.awt.event.ActionEvent evt) {//GEN-FIRST:event_jButton4ActionPerformed
+        // TODO add your handling code here:
+        eliminar();
+        
+    }//GEN-LAST:event_jButton4ActionPerformed
+
+    private void jButton3ActionPerformed(java.awt.event.ActionEvent evt) {//GEN-FIRST:event_jButton3ActionPerformed
+        // TODO add your handling code here:
+        modificarModelo();
+    }//GEN-LAST:event_jButton3ActionPerformed
 
     /**
      * @param args the command line arguments
@@ -208,47 +299,137 @@ public class modelo extends javax.swing.JInternalFrame {
     private componentes.cbxCargarDastos cbxMarcas;
     private javax.swing.JButton jButton1;
     private javax.swing.JButton jButton2;
+    private javax.swing.JButton jButton3;
+    private javax.swing.JButton jButton4;
+    private javax.swing.JButton jButton5;
     private javax.swing.JLabel jLabel1;
     private javax.swing.JLabel jLabel2;
     private javax.swing.JLabel jLabel3;
     private javax.swing.JLabel jLabel4;
     private javax.swing.JPanel jPanel1;
+    private javax.swing.JScrollPane jScrollPane1;
+    private javax.swing.JTable tblModelos;
     private javax.swing.JTextField txtCodigoMo;
     private componentes.txtMayusculas txtModelos;
     // End of variables declaration//GEN-END:variables
 
     private void insertarModelos() {
-       if (txtModelos.getText().isEmpty()||cbxMarcas.getSelectedIndex()==0) {
-           JOptionPane.showMessageDialog(this, "Seleccione una marca e inserte un modelo ");
-       }else{
-           try {
-               conexion cc = new conexion();
-               Connection cn= cc.conectar();
-               String sql="";
-               sql="INSERT INTO MODELO(MOD_CODIGO,MAR_CODIGO, COD_NOM)"
-                       + " VALUES(?,?,?)";
-               PreparedStatement psd = cn.prepareStatement(sql);
-               String codigo =txtCodigoMo.getText();
-               String dats[]=cbxMarcas.getSelectedItem().toString().split(" ");
-               String marca=dats[0];
-               System.out.println("dats "+marca);
-               String nombre = txtModelos.getText();
-               psd.setString(1, codigo);
-               psd.setString(2, marca);
-               psd.setString(3, nombre);
-               int n =psd.executeUpdate();
-               if (n>0) {
-                   JOptionPane.showMessageDialog(this, "Inserción correcta");
-               }
-           } catch (SQLException ex) {
-            JOptionPane.showMessageDialog(this, ex.getMessage());   
-           }
-       }
+        if (txtModelos.getText().isEmpty() || cbxMarcas.getSelectedIndex() == 0) {
+            JOptionPane.showMessageDialog(this, "Seleccione una marca e inserte un modelo ");
+        } else {
+            try {
+                conexion cc = new conexion();
+                Connection cn = cc.conectar();
+                String sql = "";
+                sql = "INSERT INTO MODELO(MOD_CODIGO,MAR_CODIGO, COD_NOM)"
+                        + " VALUES(?,?,?)";
+                PreparedStatement psd = cn.prepareStatement(sql);
+                String codigo = txtCodigoMo.getText();
+                String dats[] = cbxMarcas.getSelectedItem().toString().split(" ");
+                String marca = dats[0];
+                System.out.println("dats " + marca);
+                String nombre = txtModelos.getText();
+                psd.setString(1, codigo);
+                psd.setString(2, marca);
+                psd.setString(3, nombre);
+                int n = psd.executeUpdate();
+                if (n > 0) {
+                    JOptionPane.showMessageDialog(this, "Inserción correcta");
+                    cargarTabla();
+                }
+            } catch (SQLException ex) {
+                JOptionPane.showMessageDialog(this, ex.getMessage());
+            }
+        }
     }
 
     private void limpiar() {
         cargarCodigo();
         cbxMarcas.setSelectedIndex(0);
         txtModelos.setText("");
+    }
+    int ulti;
+    DefaultTableModel modelo;
+
+    private void cargarTabla() {
+        String[] titulos = {"CODIGO", "MARCA", "MODELO"
+        };
+        modelo = new DefaultTableModel(null, titulos);
+        String[] registros = new String[3];
+        try {
+
+            conexion cc = new conexion();
+            Connection cn = cc.conectar();
+            String sql = "";
+            sql = "SELECT  M.MOD_CODIGO,M.MAR_CODIGO, M.COD_NOM, A.MAR_NOM FROM modelo M INNER JOIN marca A "
+                    + "ON M.MAR_CODIGO=A.MAR_CODIGO";
+            Statement psd = cn.createStatement();
+            ResultSet rs = psd.executeQuery(sql);
+            while (rs.next()) {
+                registros[0] = rs.getString(1);
+                registros[1] = rs.getString(2)+" "+rs.getString(4);
+                registros[2] = rs.getString(3);
+
+                modelo.addRow(registros);
+            }
+            ulti = Integer.valueOf(registros[0]);
+            System.out.println("ULti: " + ulti);
+            tblModelos.setModel(modelo);
+        } catch (Exception ex) {
+            JOptionPane.showMessageDialog(null, ex);
+        }
+
+    }
+
+    private void modificarModelo() {
+        if (txtModelos.getText().isEmpty() || cbxMarcas.getSelectedIndex() == 0) {
+            JOptionPane.showMessageDialog(this, "Seleccione una marca e inserte un modelo ");
+        } else {
+            try {
+                String [] d= cbxMarcas.getSelectedItem().toString().split(" ");
+                System.out.println(d[0]);
+                conexion cc = new conexion();
+                Connection cn = cc.conectar();
+                String sql = "";
+                sql = "UPDATE modelo SET MAR_CODIGO='" + d[0]+"' , COD_NOM='"+txtModelos.getText()
+                    +  "' where MOD_CODIGO='" + txtCodigoMo.getText() + "'";
+            PreparedStatement psd = cn.prepareStatement(sql);
+            int n= psd.executeUpdate();
+            if (n > 0) {
+                JOptionPane.showMessageDialog(null, "Se modificó");
+                cargarTabla();
+              
+            
+                limpiar();
+            }
+            } catch (SQLException ex) {
+                JOptionPane.showMessageDialog(this, ex.getMessage());
+            }
+        }
+    }
+
+    private void eliminar() {
+        if(txtCodigoMo.getText().isEmpty()||Integer.valueOf(txtCodigoMo.getText())>ulti){}else{
+           if (JOptionPane.showConfirmDialog(new JInternalFrame(),
+                "Estas seguro de borrar el registro?", "Borrar resgistro", JOptionPane.YES_NO_OPTION) == JOptionPane.YES_OPTION) {
+        
+        try {
+            
+            conexion cc = new conexion();
+            Connection cn = cc.conectar();
+            String sql = "";
+            sql = "DELETE FROM modelo  WHERE MOD_CODIGO='" + txtCodigoMo.getText() + "'";
+            PreparedStatement psd = cn.prepareStatement(sql);
+            int n = psd.executeUpdate();
+            if (n > 0) {
+                JOptionPane.showMessageDialog(null, "Se elimino");
+                cargarTabla();
+                
+                limpiar();
+            }
+
+        } catch (SQLException ex) {
+            JOptionPane.showMessageDialog(null, ex);
+        }}else{}}
     }
 }
